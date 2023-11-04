9d06e4593a5d927717abcfb8d0ea9f6b2cae129e
berthold43 <thommy.johansson@sydmail.se>
2023-11-04 19:01:35 +0100

    Femman

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,16 @@
-/**
- * @type {import('gatsby').GatsbyConfig}
- */
 module.exports = {
   siteMetadata: {
-    title: `gatsby-andas`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: "My First Gatsby Site",
   },
-  plugins: [    "gatsby-plugin-image",
+  plugins: [
+    "gatsby-plugin-image",
     "gatsby-plugin-sharp",
-],
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
+  ],
 };
